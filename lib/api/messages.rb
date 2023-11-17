4183abe370ca4a5d6a99a3158b7f44f87c52e8fc
Matthew Kaliara <code@kaliara.com>
2023-11-17 02:04:55 -0500

    fixes

--- lib/api/messages.rb
+++ lib/api/messages.rb
@@ -5,13 +5,13 @@ require_relative 'recipients'
 module MonkeyBusiness
   # Abstraction of the Surveymonkey message resource and associated methods
   class Messages < ApiResource
-    def send
+    def send(options = {})
       fail_without_id
       @options.merge!(options)
       @path += '/send'
     end
 
-    def recipients
+    def recipients(options = {})
       fail_without_id
       @options.merge!(options)
       Recipients.new(@api, @options.merge(options), @path)
